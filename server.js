5956e0bc6afc107057b62a078b4ffa1541fafb54
UserTarun <saikalyantarun2001@gmail.com>
2022-08-02 13:41:05 +0530

    will be pushing to heroku

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app =express()
+const MongoClient = require('mongodb').MongoClient
 const cors = require('cors')
 const PORT = 8000
+require('dotenv').config()
 
 app.use(cors())
 
